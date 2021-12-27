2c61115c23dc3697fe43a007f7692a757dec7bee
Brenton "B-Train" Fletcher <i@bloople.net>
2021-12-27 19:57:22 +1100

    Allow user to import/export database; Clean up design of indexing activity

--- app/src/main/java/net/bloople/audiobooks/DatabaseHelper.kt
+++ app/src/main/java/net/bloople/audiobooks/DatabaseHelper.kt
@@ -2,10 +2,15 @@ package net.bloople.audiobooks
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
 
 internal object DatabaseHelper {
     private const val DB_NAME = "books"
     private lateinit var database: SQLiteDatabase
+
     private fun obtainDatabase(context: Context): SQLiteDatabase {
         val db = context.applicationContext.openOrCreateDatabase(DB_NAME, Context.MODE_PRIVATE, null)
         loadSchema(db)
@@ -31,6 +36,7 @@ internal object DatabaseHelper {
     }
 
     @JvmStatic
+    @Synchronized
     fun instance(context: Context): SQLiteDatabase {
         if (!::database.isInitialized) {
             database = obtainDatabase(context)
@@ -39,9 +45,52 @@ internal object DatabaseHelper {
     }
 
     @JvmStatic
+    @Synchronized
     fun deleteDatabase(context: Context) {
         context.applicationContext.deleteDatabase(DB_NAME)
         database = obtainDatabase(context)
+    }
+
+    @JvmStatic
+    @Synchronized
+    fun exportDatabase(context: Context, outputStream: OutputStream) {
+        val path = instance(context).use { it.path }
+        database = obtainDatabase(context)
+
+        var inputStream: InputStream? = null
+        try {
+            inputStream = FileInputStream(path)
+            val buffer = ByteArray(1024)
+            var length: Int
+            while(inputStream.read(buffer).also { length = it } > 0) {
+                outputStream.write(buffer, 0, length)
+            }
+        }
+        finally {
+            inputStream?.close()
+            outputStream.close()
+        }
+    }
+
+    @JvmStatic
+    @Synchronized
+    fun importDatabase(context: Context, inputStream: InputStream) {
+        val path = instance(context).use { it.path }
+        database = obtainDatabase(context)
+
+        var outputStream: OutputStream? = null
+        try {
+            outputStream = FileOutputStream(path)
+            val buffer = ByteArray(1024)
+            var length: Int
+            while(inputStream.read(buffer).also { length = it } > 0) {
+                outputStream.write(buffer, 0, length)
+            }
+        }
+        finally {
+            inputStream.close()
+            outputStream?.close()
+        }
     }
 
     private fun hasColumn(db: SQLiteDatabase, tableName: String, columnName: String): Boolean {
---
+++ app/src/main/java/net/bloople/audiobooks/IndexingActivity.kt
@@ -0,0 +1,119 @@
+package net.bloople.audiobooks
+
+import android.Manifest
+import net.bloople.audiobooks.DatabaseHelper.deleteDatabase
+import android.app.Activity
+import net.bloople.audiobooks.Indexable
+import android.os.Bundle
+import net.bloople.audiobooks.R
+import android.content.pm.PackageManager
+import net.bloople.audiobooks.IndexingActivity
+import net.bloople.audiobooks.IndexingTask
+import net.bloople.audiobooks.DatabaseHelper
+import android.widget.TextView.OnEditorActionListener
+import android.view.inputmethod.EditorInfo
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Environment
+import android.view.KeyEvent
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class IndexingActivity : AppCompatActivity(), Indexable {
+    private lateinit var progressBar: ProgressBar
+    private lateinit var indexButton: Button
+    private lateinit var indexRoot: String
+    private var canAccessFiles = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_indexing)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val permission = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (permission == PackageManager.PERMISSION_GRANTED) canAccessFiles = true
+        else requestPermissions(PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE)
+
+        progressBar = findViewById(R.id.indexing_progress)
+        indexButton = findViewById(R.id.index_button)
+        indexButton.setOnClickListener {
+            indexButton.setEnabled(false)
+            if (canAccessFiles) {
+                val indexer = IndexingTask(this@IndexingActivity,this@IndexingActivity)
+                indexer.execute(indexRoot)
+            }
+        }
+
+        val deleteIndexButton = findViewById<Button>(R.id.delete_index_button)
+        deleteIndexButton.setOnClickListener {
+            deleteDatabase(this@IndexingActivity)
+            Toast.makeText(this@IndexingActivity, "Index deleted.", Toast.LENGTH_SHORT).show()
+            setResult(RESULT_OK)
+        }
+
+        loadPreferences()
+
+        val indexDirectoryText: EditText = findViewById(R.id.index_directory)
+        indexDirectoryText.setText(indexRoot)
+        indexDirectoryText.setOnEditorActionListener { _, actionId, event ->
+            if(event != null && event.keyCode == KeyEvent.KEYCODE_ENTER || actionId == EditorInfo.IME_ACTION_DONE) {
+                indexRoot = indexDirectoryText.text.toString()
+                savePreferences()
+            }
+            false
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_CANCELED) finish()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == REQUEST_EXTERNAL_STORAGE && grantResults.isNotEmpty()
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED) canAccessFiles = true
+    }
+
+    private fun preferences(): SharedPreferences {
+        return applicationContext.getSharedPreferences("main", MODE_PRIVATE)
+    }
+
+    private fun loadPreferences() {
+        val preferences = preferences()
+        indexRoot = preferences.getString("index-root", Environment.getExternalStorageDirectory().absolutePath).toString()
+    }
+
+    private fun savePreferences() {
+        val editor = preferences().edit()
+        editor.putString("index-root", indexRoot)
+        editor.apply()
+    }
+
+    override fun onIndexingProgress(progress: Int, max: Int) {
+        progressBar.progress = progress
+        progressBar.max = max
+    }
+
+    override fun onIndexingComplete(count: Int) {
+        setResult(RESULT_OK)
+        indexButton.isEnabled = true
+        Toast.makeText(
+            this@IndexingActivity, "Indexing complete, $count audiobooks indexed.",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    companion object {
+        // Storage Permissions
+        private const val REQUEST_EXTERNAL_STORAGE = 1
+        private val PERMISSIONS_STORAGE = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    }
+}
